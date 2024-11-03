c722793c353e35763a95647883feb82df200a7fb
강은찬 <kangeunchan080310@gmail.com>
2024-11-04 00:24:48 +0900

    init : first commit

---
+++ src/main/java/labs/prime/primebackend/PrimeBackendApplication.java
@@ -0,0 +1,13 @@
+package labs.prime.primebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/labs/prime/primebackend/PrimeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package labs.prime.primebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
