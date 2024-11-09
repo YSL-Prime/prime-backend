620da85f1852b485b54a2a03c989da3d8a6fe704
Hannah <sunday0206@dsm.hs.kr>
2024-11-10 00:37:59 +0900

    feat : Update Post controller

--- src/main/java/labs/prime/primebackend/domain/post/presentation/controller/PostController.java
+++ src/main/java/labs/prime/primebackend/domain/post/presentation/controller/PostController.java
@@ -5,6 +5,7 @@ import labs.prime.primebackend.domain.post.presentation.dto.request.PostCreateRe
 import labs.prime.primebackend.domain.post.presentation.dto.request.PostUpdateRequest;
 import labs.prime.primebackend.domain.post.presentation.dto.response.PostListAdminViewResponse;
 import labs.prime.primebackend.domain.post.presentation.dto.response.PostListResponse;
+import labs.prime.primebackend.domain.post.presentation.dto.response.ViewPostAdminResponse;
 import labs.prime.primebackend.domain.post.presentation.dto.response.ViewPostResponse;
 import labs.prime.primebackend.domain.post.service.*;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/post")
+@RequestMapping()
 public class PostController {
     private final PostDeleteService deleteService;
     private final PostViewService viewService;
@@ -22,37 +23,42 @@ public class PostController {
     private final PostAllowService allowService;
     private final PostUpdateService updateService;
 
-    @PostMapping("/write")
+    @PostMapping("/post/write")
     private void postWrite(@RequestBody PostCreateRequest request) {
         createService.postCreate(request);
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/post/{id}")
     private void postDelete(@PathVariable("id") UUID postId, @RequestHeader("Authorization") String token) {
         deleteService.postDelete(postId, token);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/post/{id}")
     private ViewPostResponse postView(@PathVariable("id") UUID id) {
         return viewService.viewPostDetails(id);
     }
 
-    @GetMapping("/posts")
+    @GetMapping("/post/all")
     private PostListResponse postsView() {
         return viewService.postsView();
     }
 
-    @GetMapping("/admin/posts")
-    private PostListAdminViewResponse postsAllowView() {
-        return viewService.postsAllowView();
+    @GetMapping("/admin/post/{id}")
+    private ViewPostAdminResponse postAdminViewDetails(@PathVariable("id") UUID id, @RequestHeader("Authorization") String token) {
+        return viewService.viewPostAdminDetails(id, token);
     }
 
-    @PatchMapping("/{id}")
+    @GetMapping("/admin/post/all")
+    private PostListAdminViewResponse postsAdminView(@RequestHeader("Authorization") String token) {
+        return viewService.viewAdminPosts(token);
+    }
+
+    @PatchMapping("/post/update/{id}")
     private void postUpdate(@PathVariable("id") UUID id, @RequestBody PostUpdateRequest request, @RequestHeader("Authorization") String token) {
         updateService.updatePost(id, request, token);
     }
 
-    @PatchMapping("/admin/{id}")
+    @PatchMapping("/admin/post/{id}")
     private void postAllow(@PathVariable("id") UUID id, @RequestBody PostAllowRequest request, @RequestHeader("Authorization") String token) {
         allowService.allow(id, request, token);
     }
