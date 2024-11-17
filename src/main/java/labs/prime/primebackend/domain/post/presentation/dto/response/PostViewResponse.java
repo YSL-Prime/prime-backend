36af44b48215b5bd29156a2a59cf8095d4c54a0c
Hannah <sunday0206@dsm.hs.kr>
2024-11-17 12:14:34 +0900

    feat: Post Request 수정

--- src/main/java/labs/prime/primebackend/domain/post/presentation/dto/response/PostViewResponse.java
+++ src/main/java/labs/prime/primebackend/domain/post/presentation/dto/response/PostViewResponse.java
@@ -2,13 +2,12 @@ package labs.prime.primebackend.domain.post.presentation.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
-public class ViewPostResponse {
+public class PostViewResponse {
     private String title;
     private LocalDate creationDate;
     private String content;
