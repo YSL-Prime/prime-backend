41b41358bc65ff1dbad7c00bf7dedb38f2e745ef
Hannah <sunday0206@dsm.hs.kr>
2024-11-17 12:15:43 +0900

    feat: PostListResponse 수정

--- src/main/java/labs/prime/primebackend/domain/post/presentation/dto/response/PostListResponse.java
+++ src/main/java/labs/prime/primebackend/domain/post/presentation/dto/response/PostListResponse.java
@@ -1,12 +1,14 @@
 package labs.prime.primebackend.domain.post.presentation.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 public class PostListResponse {
     private final List<PostListElement> postList;
+
+    public PostListResponse(List<PostListElement> postList) {
+        this.postList = postList;
+    }
 }
