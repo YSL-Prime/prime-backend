7c0ce3d731d5c084dc9533e3418fee6868be71f1
Hannah <sunday0206@dsm.hs.kr>
2024-11-17 12:05:48 +0900

    feat: PostRepository 수정

--- src/main/java/labs/prime/primebackend/domain/post/entity/repository/PostRepository.java
+++ src/main/java/labs/prime/primebackend/domain/post/entity/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package labs.prime.primebackend.domain.post.entity.repository;
 
 import labs.prime.primebackend.domain.post.entity.Post;
-import labs.prime.primebackend.domain.post.presentation.dto.response.PostListElement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, UUID> {
-    Optional<Post> findById(UUID id);
-    List<PostListElement> findAllByAllow(Boolean allow);
+    List<Post> findAllByAllow(Boolean allow);
 }
+
